fdc585bdc4408d7269e264ecf44858b5c26363c0
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:44:43 +0100

    Extend from Ogone library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,21 +1,9 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_ING_IDealInternetKassa_Integration {
+class Pronamic_WP_Pay_Gateways_ING_IDealInternetKassa_Integration extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Integration {
 	public function __construct() {
 		$this->id       = 'ing-ideal-internetkassa';
 		$this->name     = 'ING - iDEAL Internet Kassa';
 		$this->provider = 'ing';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Gateway';
 	}
 }
